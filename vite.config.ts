80cf6d750a372099a65c0afd88ae44b5a0e7f4c1
ruimachado23 <rui.faria.machado@gmail.com>
2025-05-22 01:36:59 +0100

    connect stations creation to forms

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,15 @@ export default defineConfig({
     plugins: [react(), tailwindcss()],
       server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://stations-management:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
