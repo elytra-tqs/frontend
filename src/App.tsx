fddf50d0544394712960185f3e8f23de174995b1
ruimachado23 <rui.faria.machado@gmail.com>
2025-05-22 10:25:25 +0100

    dashboard: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import StationsPage from './pages/manage_stations/StationsPage';
 import StationDetails from './pages/manage_stations/StationDetails';
 import Dashboard from './pages/Dashboard';
+import EVDriverPage from './pages/evdriver/EVDriverPage';
+import OperatorPage from './pages/operator/OperatorPage';
 import { StationsProvider } from "./contexts/StationsContext";
 import { ChargersProvider } from "./contexts/ChargersContext";
 
@@ -12,6 +14,16 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            
+            {/* EV Driver Routes */}
+            <Route path="/evdriver" element={<EVDriverPage />} />
+            
+            {/* Operator Routes */}
+            <Route path="/operator" element={<OperatorPage />} />
+            <Route path="/operator/stations" element={<StationsPage />} />
+            <Route path="/operator/stations/:stationId" element={<StationDetails />} />
+            
+            {/* Redirect old routes for backward compatibility */}
             <Route path="/stations" element={<StationsPage />} />
             <Route path="/stations/:stationId" element={<StationDetails />} />
           </Routes>
